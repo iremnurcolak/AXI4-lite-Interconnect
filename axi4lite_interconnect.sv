524b4b530b56a78ec69d8afcab6fc6b3e2686fad
iremnurcolak <iremnurcolak34@gmail.com>
2021-08-25 23:22:17 +0300

    duzeltme

--- axi4lite_interconnect.sv
+++ axi4lite_interconnect.sv
@@ -2,9 +2,11 @@
 
 
 module axilite_interconnect  
-#(  parameter [511:0] base_address_of_masters = 0, //master 15'ten 0'a
-    parameter [511:0] ranges_of_masters       = 0, //master 15'ten 0'a     
-    parameter numOfMasters                    = 16 //maximum 16 olabilir
+#(  
+parameter numOfMasters                    = 16, //maximum 16 olabilir
+parameter [32*numOfMasters-1:0] base_address_of_masters = 0, //master 15'ten 0'a
+    parameter [32*numOfMasters-1:0] ranges_of_masters       = 0 //master 15'ten 0'a     
+  
  )
 (
 
@@ -37,33 +39,33 @@ module axilite_interconnect
     /////////////////////////////////////////////////////////////////
     
     //master read channel
-    output reg [511:0]  m_axi_araddr  ,
-    input      [15:0]   m_axi_arready ,
-    output reg [15:0]   m_axi_arvalid ,
-    input      [511:0]  m_axi_rdata   ,
-    output reg  [15:0]  m_axi_rready  , //bu neden output(anlamis olabilirim)
+    output reg [32*numOfMasters-1:0]  m_axi_araddr  ,
+    input      [numOfMasters-1:0]   m_axi_arready ,
+    output reg [numOfMasters-1:0]   m_axi_arvalid ,
+    input      [32*numOfMasters-1:0]  m_axi_rdata   ,
+    output reg  [2*numOfMasters-1:0]  m_axi_rready  , //bu neden output(anlamis olabilirim)
     
-    output reg [47:0]   m_axi_arprot  , //3 bit
-    input      [31:0]   m_axi_rresp   , //2 bit
-    input      [15:0]   m_axi_rvalid  ,
+    output reg [3*numOfMasters-1:0]   m_axi_arprot  , //3 bit
+    input      [2*numOfMasters-1:0]   m_axi_rresp   , //2 bit
+    input      [numOfMasters-1:0]   m_axi_rvalid  ,
 
     //m
-    output reg [511:0]  m_axi_awaddr  ,
-    input      [15:0]  m_axi_awready ,
-    output reg  [15:0]       m_axi_awvalid ,
-    output reg [511:0]  m_axi_wdata   ,
-    input      [15:0]        m_axi_wready  ,
-    output reg [63:0]   m_axi_wstrb   , //4 bit
-    output reg   [15:0]      m_axi_wvalid  ,
-    output reg [47:0]   m_axi_awprot  , //3 bit
+    output reg [32*numOfMasters-1:0]  m_axi_awaddr  ,
+    input      [numOfMasters-1:0]  m_axi_awready ,
+    output reg  [numOfMasters-1:0]       m_axi_awvalid ,
+    output reg [32*numOfMasters-1:0]  m_axi_wdata   ,
+    input      [numOfMasters-1:0]        m_axi_wready  ,
+    output reg [4*numOfMasters-1:0]   m_axi_wstrb   , //4 bit
+    output reg   [numOfMasters-1:0]      m_axi_wvalid  ,
+    output reg [3*numOfMasters-1:0]   m_axi_awprot  , //3 bit
 
-    input      [31:0]   m_axi_bresp   ,
-    input       [15:0]       m_axi_bvalid  ,   
-    output reg  [15:0]       m_axi_bready  
+    input      [2*numOfMasters-1:0]   m_axi_bresp   ,
+    input       [numOfMasters-1:0]       m_axi_bvalid  ,   
+    output reg  [numOfMasters-1:0]       m_axi_bready  
  );
  
-    reg [511:0] base_address_of_masters_r;
-    reg [511:0] ranges_of_masters_r      ;
+    reg [32*numOfMasters-1:0] base_address_of_masters_r;
+    reg [32*numOfMasters-1:0] ranges_of_masters_r      ;
     
     integer i,m,n;
     initial begin
@@ -82,8 +84,8 @@ module axilite_interconnect
         s_axi_wready  =1;
         
         for(m=0;m<numOfMasters;m++)begin
-            base_address_of_masters_r[m+:32] = base_address_of_masters[m+:32];
-            ranges_of_masters_r[m+:32] = ranges_of_masters[m+:32];
+            base_address_of_masters_r[m*32+:32] = base_address_of_masters[m+:32];
+            ranges_of_masters_r[m*32+:32] = ranges_of_masters[m+:32];
            
         end
         
@@ -116,9 +118,9 @@ module axilite_interconnect
         
         if(s_axi_awvalid==1)begin
             for(n=0;n<numOfMasters;n++)begin
-               if($signed(s_axi_awaddr)<=$signed(ranges_of_masters[n+:32]+base_address_of_masters_r[n+:32]-1) && s_axi_awaddr>=base_address_of_masters_r[n+:32]) begin       
+               if($signed(s_axi_awaddr)<=$signed(ranges_of_masters[n*32+:32]+base_address_of_masters_r[n*32+:32]-1) && s_axi_awaddr>=base_address_of_masters_r[n*32+:32]) begin       
                    master_no_write=n;
-                   master_addr_write=s_axi_awaddr-base_address_of_masters_r[n+:32];
+                   master_addr_write=s_axi_awaddr-base_address_of_masters_r[n*32+:32];
                end
             end
         end
@@ -128,27 +130,27 @@ module axilite_interconnect
         if(s_axi_aresetn)begin
          //   $display("%d",master_no_write);
             if(s_axi_arvalid && s_axi_rready && m_axi_arready[master_no_read] && reading==0)begin
-                m_axi_araddr[master_no_read+:32]<=master_addr_read;
+                m_axi_araddr[master_no_read*32+:32]<=master_addr_read;
                 m_axi_arvalid[master_no_read]<=1;
                 master_no_read_e<=master_no_read;
                 s_axi_arready<=0;
                 m_axi_rready[master_no_read]<=1;
                 reading<=1;
-                m_axi_arprot[master_no_read+:3]<=s_axi_arprot;
+                m_axi_arprot[master_no_read*3+:3]<=s_axi_arprot;
 
             end
             if(s_axi_awvalid && s_axi_wvalid && m_axi_awready[master_no_write] && writing==0 && m_axi_wready[master_no_write])begin
-                m_axi_awaddr[master_no_write+:32]<=master_addr_write;
+                m_axi_awaddr[master_no_write*32+:32]<=master_addr_write;
                 m_axi_awvalid[master_no_write]<=1;
                 master_no_write_e<=master_no_write;
                 s_axi_awready<=0;
-                m_axi_wstrb[master_no_write+:4]<=s_axi_wstrb;
+                m_axi_wstrb[master_no_write*4+:4]<=s_axi_wstrb;
                 m_axi_bready[master_no_write]<=1;
                 s_axi_wready<=0;
-                m_axi_wdata[master_no_write+:32]<=s_axi_wdata;
+                m_axi_wdata[master_no_write*32+:32]<=s_axi_wdata;
                 m_axi_wvalid[master_no_write]<=1;
                 writing<=1;
-                m_axi_awprot[master_no_write+:3]<=s_axi_awprot;
+                m_axi_awprot[master_no_write*3+:3]<=s_axi_awprot;
 
             end
         end
@@ -183,16 +185,16 @@ module axilite_interconnect
                 s_axi_arready<=1;
             end
             if(reading==1 && m_axi_rvalid[master_no_read_e] )begin
-                s_axi_rdata<=m_axi_rdata[master_no_read_e+:32];
+                s_axi_rdata<=m_axi_rdata[master_no_read_e*32+:32];
                 s_axi_rvalid<=1;
-                s_axi_rresp<=m_axi_rresp[master_no_read_e+:2];
+                s_axi_rresp<=m_axi_rresp[master_no_read_e*2+:2];
                 reading<=0;
                 
             end
             if(writing==1 && m_axi_bvalid[master_no_write_e])begin
                 m_axi_bready[master_no_write_e]<=0;
                 writing<=0;
-                s_axi_bresp<=m_axi_bresp[master_no_write_e+:2];
+                s_axi_bresp<=m_axi_bresp[master_no_write_e*2+:2];
                 s_axi_bvalid<=1;
    //             s_axi_awready<=1;
     //            s_axi_wready<=1;
